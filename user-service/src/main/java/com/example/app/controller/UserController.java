287694351aad7ecb5d093616c6a1bdd8e3d20d71
LahiruTjay <lahiru.tjay@gmail.com>
2018-07-13 22:42:55 +0530

    pom files updated. Added eureka client

--- user-service/src/main/java/com/example/app/controller/UserController.java
+++ user-service/src/main/java/com/example/app/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.example.app.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-	@GetMapping("/")
+	@RequestMapping("/")
 	public String getUser() {
 		return "Hello World";
 	}
