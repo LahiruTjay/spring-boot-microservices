26fe6187ee30d90300d61e634192b6b2287576bc
LahiruTjay <lahiru.tjay@gmail.com>
2018-07-14 21:59:15 +0530

    User service application.yml updated.

--- user-service/src/main/java/com/example/app/controller/UserController.java
+++ user-service/src/main/java/com/example/app/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-	@RequestMapping("/")
+	@RequestMapping("/get/user")
 	public String getUser() {
 		return "Hello World";
 	}
