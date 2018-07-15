af0282dbfd959d9db68dcaf8252340bc84a02a1d
LahiruTjay <lahiru.tjay@gmail.com>
2018-07-15 19:35:47 +0530

    Added hystrix circuit breaker to test client

--- api-gateway/src/main/java/com/example/gateway/controller/RestClientController.java
+++ api-gateway/src/main/java/com/example/gateway/controller/RestClientController.java
@@ -1,10 +1,12 @@
 package com.example.gateway.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 
 @RestController
 public class RestClientController {
@@ -12,10 +14,15 @@ public class RestClientController {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@HystrixCommand(fallbackMethod = "fallbackMethod")
 	@RequestMapping("/hello")
 	public String helloClient() {
 		String url = "http://user-service/user";
 		return restTemplate.getForObject(url, String.class);
+	}
+	
+	public String fallbackMethod() {
+		return "fall back method";
 	}
 
 }
